b806788d895e51012e04d28a7e21f65e4972192c
Tomas Correa <79479585+tcorreae96@users.noreply.github.com>
2024-08-02 19:14:59 -0400

    calculator finished

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const screenDisplay = document.querySelector("#screen");
 const buttons = document.querySelectorAll("button");
+let isReadyForNextNumber = false;
 let seq = [];
 
 buttons.forEach(button => button.addEventListener("click", handleClick));
@@ -7,32 +8,39 @@ buttons.forEach(button => button.addEventListener("click", handleClick));
 function handleClick(e) {
 	switch (e.target.innerText) {
 		case "+":
-			console.log("pressed +");
+			addSeq(screenDisplay.innerText, e.target.innerText);
+			isReadyForNextNumber = true;
 			break;
 		case "-":
-			console.log("pressed -");
+			addSeq(screenDisplay.innerText, e.target.innerText);
+			isReadyForNextNumber = true;
 			break;
 		case "/":
-			console.log("pressed /");
+			addSeq(screenDisplay.innerText, e.target.innerText);
+			isReadyForNextNumber = true;
 			break;
 		case "*":
-			console.log("pressed *");
+			addSeq(screenDisplay.innerText, e.target.innerText);
+			isReadyForNextNumber = true;
 			break;
 		case "=":
-			console.log("pressed =");
-			console.log(screenDisplay.innerText);
+			addSeq(screenDisplay.innerText, e.target.innerText);
+			isReadyForNextNumber = true;
+			operate();
 			break;
 		case "C":
-			console.log("pressed C");
 			clearDisplay();
 			break;
 		default:
 			updateDisplay(e.target.innerText);
 			break;
 	}
-	seq.push(e.target.innerText);
+}
+
+function addSeq(num, op) {
+	seq.push(num);
+	seq.push(op);
 	console.log(seq);
-	
 }
 
 function checkDisplay() {
@@ -51,44 +59,57 @@ function clearDisplay() {
 	screenDisplay.innerText = "0";
 }
 
+function clearSeq() {
+	seq = [];
+}
+
 
 function updateDisplay(num) {
+	if (isReadyForNextNumber) clearDisplay();
 	screenDisplay.innerText += num;
 	checkDisplay();
 	checkDot();
+	isReadyForNextNumber = false;
 	return num; 
 }
 
-function add(num1, num2) {
-	return num1 + num2;
+function add(arr) {
+	return +arr[0] + +arr[2];
 }
 
-function subtract(num1, num2) {
-	return num1 - num2;
+function subtract(arr) {
+	return arr[0] - arr[2];
 }
 
-function multiply(num1, num2) {
-	return num1 * num2;
+function multiply(arr) {
+	return arr[0] * arr[2];
 }
 
-function divide(num1, num2) {
-	if (num1 === 0 || num2 === 0) {
+function divide(arr) {
+	if (arr[0] === 0 || arr[2] === 0) {
 		return alert("This is gonna take a while, please wait sitted.");
 	}
-	return num1 / num2;
+	return arr[0] / arr[2];
 }
 
-function operate(arr) {
-	switch (arr[1]) {
-		case "+":
-			return add(arr);
-		case "-":
-			return subtract(arr);
-		case "*":
-			return multiply(arr); 
-		case "/":
-			return divide(arr); 
-		default:
-			break;
+function operate() {
+	console.log('inside operate');
+	if (seq.length === 4) {
+		switch (seq[1]) {
+			case "+":
+				screenDisplay.innerText = add(seq);
+				break;
+			case "-":
+				screenDisplay.innerText = subtract(seq);
+				break;
+			case "*":
+				screenDisplay.innerText = multiply(seq); 
+				break;
+			case "/":
+				screenDisplay.innerText = divide(seq); 
+				break;
+		}
+		clearSeq();
 	}
+	clearSeq();
 }
